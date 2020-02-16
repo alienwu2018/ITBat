33f09ff24c3598c8007518c14911c8f9a0cfaad1
aleinwu2018 <582946945@qq.com>
2020-02-16 17:51:58 +0800

    修改api

--- Backed/pkg/e/category.go
+++ Backed/pkg/e/category.go
@@ -1,5 +1,5 @@
 package e
 
-//大类
+//类别
 var BingCategory = []string{"front-end-development", "mobile-development", "data-mining", "machine-learning", "big-data-and-cloud-computing", "back-end-development", "search-engine", "fundamentals-of-compiling", "hardware", "operating-system", "blockchain", "message-queue", "game-development-and-design", "internet-and-science", "network-programming", "algorithm-and-data-structure", "ops", "cyber-security", "interactive-design", "software-development-and-engineering", "database", "computer-science", "uncategorized"}
 var SmallCategory = []string{"c++", "go", "python", "other", "java", "c", "php", "c%23", "rust", "erlang", "ios", "android", "javascript", "framework-and-library", "node-js", "html-and-css", ""}
--- Backed/pkg/util/category.go
+++ Backed/pkg/util/category.go
@@ -1,10 +1,17 @@
 package util
 
-import "ITBat/model/book"
+import (
+	"ITBat/model/book"
+)
+
+type Tree struct {
+	Label    string
+	Children []interface{}
+}
 
 //将书籍类型进行分类
 //ex:{front:{js,css,html},back{java,python}}
-func DoCategory(books []book.Book) map[string][]string {
+func DoCategory(books []book.Book) []Tree {
 	var result = make(map[string][]string)
 	flag := ""
 	for _, book := range books {
@@ -17,5 +24,26 @@ func DoCategory(books []book.Book) map[string][]string {
 		}
 		result[book.BigCategory] = tmp
 	}
-	return result
+	var categories []Tree
+	count := 0
+	for k, _ := range result {
+		var t Tree
+		t.Label = k
+		for _, v := range result[k] {
+			if len(v) > 0 {
+				t.Children = append(t.Children, map[string]string{"label": v})
+			}
+		}
+		count++
+		categories = append(categories, t)
+	}
+	//排序
+	for i := 0; i < len(categories)-1; i++ {
+		for j := 0; j < len(categories)-1-i; j++ {
+			if len(categories[j].Children) < len(categories[j+1].Children) {
+				categories[j], categories[j+1] = categories[j+1], categories[j]
+			}
+		}
+	}
+	return categories
 }
